8bf84e6973fedc72ebaf5f430f2ffa45665ca4ed
Alexandr <valverin95@gmail.com>
2021-07-31 19:36:12 +0300

    feat: реализован запрос на логин с формы

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UserFacade } from "@state/user/user.facade";
+import { UserLoginDto } from "@models/user";
+import { catchError } from "rxjs/operators";
+import { HttpErrorResponse } from "@angular/common/http";
+import { throwError } from "rxjs";
 
 enum LoginValidatorsEnum {
   name = 'invalidLogin'
@@ -24,6 +29,7 @@ export class LoginPageComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private user: UserFacade,
   ) {
   }
 
@@ -38,7 +44,22 @@ export class LoginPageComponent implements OnInit {
     });
   }
 
-  login() {
+  login(): void {
+    const dto = this.createLoginDto();
+    this.user.login(dto).pipe(
+      catchError((err: HttpErrorResponse) => {
+        alert(err.message);
+        return throwError(err);
+      }),
+    ).subscribe(data => {
+      console.log(data);
+    });
+  }
 
+  private createLoginDto(): UserLoginDto {
+    const name = this.form.value.name;
+    const password = this.form.value.password;
+
+    return {name, password};
   }
 }
