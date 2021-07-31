cf7fc56664058280bb48c74a9ce5727abc41fbc9
Alexandr <valverin95@gmail.com>
2021-07-31 11:15:30 +0300

    feat: добавлен шаблон для логина

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+enum LoginValidatorsEnum {
+  name = 'invalidLogin'
+}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit(): void {
+  get nameInput() {
+    return this.form.get('name');
   }
 
+  get invalidError() {
+    return this.nameInput?.errors?.[LoginValidatorsEnum.name];
+  }
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = this.fb.group({
+      name: [null, Validators.required],
+      password: [null, Validators.required],
+    });
+  }
+
+  login() {
+
+  }
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
 })
 export class LoginModule { }
