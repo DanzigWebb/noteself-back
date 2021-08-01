6cc0045677c6666d7b565a3f4d5cf1df76d08aa7
Alexandr <valverin95@gmail.com>
2021-08-01 15:32:22 +0300

    feat: создан навбар

--- src/app/core/template/template.module.ts
+++ src/app/core/template/template.module.ts
@@ -5,12 +5,14 @@ import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
     HeaderComponent,
+    NavbarComponent,
   ],
   imports: [
     CommonModule,
