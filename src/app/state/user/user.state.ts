01d79ca3c67caea59a925c043e1ec8c80a59a91e
Alexandr <valverin95@gmail.com>
2021-08-11 14:57:33 +0300

    fix: загрузка тем и заметок после логина

--- src/app/state/user/user.state.ts
+++ src/app/state/user/user.state.ts
@@ -3,8 +3,11 @@ import { Action, Selector, State, StateContext } from '@ngxs/store';
 import { UserActions } from './user.actions';
 import { UserDto } from '@models/user.interface';
 import { ApiService } from '@services/api.service';
-import { tap } from 'rxjs/operators';
+import { switchMap, take, tap } from 'rxjs/operators';
 import { USER_STORAGE, UserStorage } from '@shared/storages/user.storage';
+import { SubjectFacade } from "@state/subject/subject.facade";
+import { NoteFacade } from "@state/note/note.facade";
+import { forkJoin } from "rxjs";
 
 export interface UserStateModel {
   isLogin: boolean;
@@ -40,6 +43,8 @@ export class UserState {
   constructor(
     @Inject(USER_STORAGE) private storage: UserStorage,
     private api: ApiService,
+    private subjectFacade: SubjectFacade,
+    private noteFacade: NoteFacade,
   ) {
   }
 
@@ -50,6 +55,11 @@ export class UserState {
         setState({isLogin: true, user});
         this.storage.setUser(user);
       }),
+      switchMap(() => forkJoin([
+        this.noteFacade.getAll().pipe(take(1)),
+        this.subjectFacade.getAll().pipe(take(1)),
+      ])),
+      tap(() => this.noteFacade.checkDefaultSubject())
     );
   }
 
